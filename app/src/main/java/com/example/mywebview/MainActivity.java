52fbaae96559bb40967695cc46aed85a3ce7a238
Bimz <edotayam@gmail.com>
2025-08-17 22:39:28 +0700

    Create MainActivity.java

---
+++ app/src/main/java/com/example/mywebview/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.mywebview;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+
+        // 🔥 Ganti URL ini dengan dashboard online kamu
+        webView.loadUrl("https://your-dashboard-url.com");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
